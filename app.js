a09279f52b6faf7d40f23fe4418a5e6ec362e4ed
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-07-20 15:03:53 +0530

    added basic functioning and routing

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Todo = require('./model/todo')
+const Todo = require('./model/todo');
+const path = require('path');
+const { urlencoded } = require('express');
 const app = express();
 
-
+//db connection
 mongoose.connect('mongodb://localhost:27017/newTodoDb')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connections error"));
 db.once("open", ()=> {
     console.log("Database Connected");
+})
+
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+//routes
+
+app.get('/todo', async(req, res)=> {
+    const todos = await Todo.find({});
+    res.render('todo', {todos});
+})
+
+app.post('/todo', async(req, res) => {
+    // console.log(req.body)
+    const todotask = new Todo(req.body.todo)
+    await todotask.save();
+    res.redirect('/todo');
 })
 
 app.listen(3030, () => {
--- model/todo.js
+++ model/todo.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema =  mongoose.Schema
 
 const todoSchema = new Schema({
-    Task: String,
-    status:String
+    task: String,
+    status:String,
+    importance:Number
 }) 
 
 module.exports = mongoose.model('Todo', todoSchema);
